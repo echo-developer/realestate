baca518ec69576a901f2f1bf3d5e886beb28c5d9
originate-soft-del <55586540+originate-soft@users.noreply.github.com>
2024-11-14 15:14:39 +0530

    ssa upd

--- resources/views/Admin/Users/index.blade.php
+++ resources/views/Admin/Users/index.blade.php
@@ -45,6 +45,9 @@
         @if (session('success_msg'))
             <div class="alert alert-{{ session('message_type') }}">
                 {{ session('success_msg') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
         @endif
         <div class="main-card mb-3 card">
