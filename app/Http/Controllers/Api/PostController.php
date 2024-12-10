d233def68f15722b1eaee51876f0652a12c8b069
Soumyadip <143384729+echo-developer@users.noreply.github.com>
2024-12-10 16:05:23 +0530

    Update PostController.php

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,26 +9,36 @@ class PostController extends Controller
 {
     public function ImageUpload(Request $req)
     {
+        
         $req->validate([
-            'images.*' => 'required|image|mimes:jpg,jpeg,png,gif|max:2048', // Validate each file in the array
+            'images' => 'required',  
+            'images.*' => 'image|mimes:jpg,jpeg,png,gif|max:2048',  
         ]);
-
+    
+        $uploadedFiles = [];
+    
+      
         if ($req->hasFile('images')) {
-            $uploadedFiles = [];
-
-            foreach ($req->file('images') as $file) {
+            $images = $req->file('images');
+            
+            
+            $images = is_array($images) ? $images : [$images];
+    
+            foreach ($images as $file) {
                 $fileName = time() . '-' . $file->getClientOriginalName();
                 $file->move(public_path('property_images'), $fileName);
                 $uploadedFiles[] = $fileName;
             }
-
+    
             return response()->json([
                 'status' => 1,
                 'message' => 'Files successfully uploaded',
                 'files' => $uploadedFiles
             ]);
         }
-
+    
         return response()->json(['error' => 'No files uploaded'], 400);
     }
+    
+    
 }
