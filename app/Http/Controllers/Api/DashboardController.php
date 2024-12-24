762ad87b72b4252312e61d297a23e8e08fe46f99
originate-soft-del <55586540+originate-soft@users.noreply.github.com>
2024-12-24 13:50:54 +0530

    favorite list

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -441,4 +441,102 @@ class DashboardController extends Controller
             ]);
         }
     }
+
+    public function My_fav_Property_List(Request $request)
+    {
+
+        try {
+            $user_id = $request->input('user_id');
+            $currentPage = $request->input('current_page', 1);
+            $limit = $request->input('limit', 10);
+            $recentOffset = ($currentPage - 1) * $limit;
+
+            if (empty($user_id)) {
+                return response()->json([
+                    'status' => 0,
+                    'message' => 'NO USER ID FOUND',
+                ]);
+            }
+
+            $properties = $this->apiModel->myFavoritePropertyList($user_id);
+
+            Log::info("Request in AddmyFavoriteProperty:\n" . json_encode($properties, JSON_PRETTY_PRINT));
+            $formattedProperties = $properties->map(function ($property) {
+                $galleries = [];
+
+                if (!empty($property->galleries)) {
+                    $galleryEntries = explode(';;', $property->galleries);
+                    Log::info("Request in AddmyFavoriteProperty:\n" . json_encode($galleryEntries, JSON_PRETTY_PRINT));
+                    $galleries = [];
+
+                    foreach ($galleryEntries as $entry) {
+                        $parts = explode('||', $entry);
+
+                        Log::info("Request in AddmyFavoriteProperty:\n" . json_encode($parts, JSON_PRETTY_PRINT));
+
+                        $images = isset($parts[2]) ? explode(',', $parts[2]) : [];
+                        $imagesWithUrl = array_map(function ($image) {
+                            return url('property_images/' . $image);
+                        }, $images);
+
+                        $galleries[] = [
+                            'gallery_name' => $parts[0] ?? null,
+                            'gallery_caption' => $parts[1] ?? null,
+                            'images' => $imagesWithUrl,
+                        ];
+                    }
+                }
+
+
+                return [
+                    'property_id' => $property->property_id,
+                    'user' => get_user_name($property->uid),
+                    'property_size' => $property->carpet_area * $property->plot_area,
+                    'property_name' => $property->property_name,
+                    'slug' => $property->slug,
+                    'views' => $property->views,
+                    'is_featured' => $property->is_featured,
+                    'is_populer' => $property->is_populer,
+                    'parking_ability' => $property->parking_ability,
+                    'property_type_for' => get_name_by_id('pref_property_sub_category_names', 'sub_category_id', $property->property_type_for, 'en'),
+                    'bedrooms' => $property->bedrooms,
+                    'bathroom' => $property->bathrooms,
+                    'price' => $property->price_currency . " " . $property->expected_price,
+                    'created_at' => $property->created_at,
+                    'address' => $property->property_address,
+                    'galleries' => $galleries,
+                ];
+            });
+
+            $favoriteProperties = $formattedProperties
+                ->sortByDesc('created_at')
+                ->skip($recentOffset)
+                ->take($limit)
+                ->values();
+
+            if ($properties->isEmpty()) {
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'No properties found',
+                    'data' => [],
+                ]);
+            }
+
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Properties fetched successfully',
+                'data' => [
+                    'favorite_properties' => $favoriteProperties,
+                ],
+            ]);
+        } catch (\Exception $e) {
+            Log::error('Error in UpdateAmenities: ' . $e->getMessage());
+
+            return response()->json([
+                'status' => 0,
+                'message' => 'An unexpected error occurred. Please try again later.',
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -61,6 +61,7 @@ Route::controller(DashboardController::class)->group(function () {
     Route::get('/get_property_amenity', 'PropertyAmenities')->name('get.property.amenities');
     Route::post('/update_property_amenity', 'UpdateAmenities')->name('update.property.amenities');
     Route::post('/add_my_fav_property', 'Add_fav_Property')->name('add.fav.property');
+    Route::get('/my_fav_property_list', 'My_fav_Property_List')->name('my.fav.property');
 });
 
 Route::controller(SeachController::class)->group(function(){
