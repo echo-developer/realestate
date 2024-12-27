6abe367960b0200001685d66de62d27c65f54099
originate-soft-del <55586540+originate-soft@users.noreply.github.com>
2024-12-27 19:38:13 +0530

    commit

---
+++ app/Http/Controllers/Api/PropertyUpdateControler.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\PrefPropertyLocation;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class PropertyUpdateControler extends Controller
+{
+    public function UpdateProperty(Request $request)
+    {
+        Log::info("Request in AddmyFavoriteProperty:\n" . json_encode($request->all(), JSON_PRETTY_PRINT));
+        try {
+
+            $this->Updateaddress($request);
+            $this->UpdateAdditionalData($request);
+
+
+            return response()->json([
+                'status' => 1,
+                'message' => 'property Updated successfully',
+            ], 201);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 0,
+                'message' => 'Failed to get property',
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+
+    public function Updateaddress($req)
+    {
+        Log::info("Request in inside Updateaddress:\n" . json_encode($req->all(), JSON_PRETTY_PRINT));
+        DB::beginTransaction();
+
+        try {
+
+            $datatoupdate = [
+                'property_address' => $req->address,
+                'locality' => $req->locality,
+            ];
+
+            DB::table('pref_properties_location')->where('pid', $req->property_id)->update($datatoupdate);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 0,
+                'message' => 'Failed to get property',
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function UpdateAdditionalData($req)
+    {
+        Log::info("Request in inside Updateaddress:\n" . json_encode($req->all(), JSON_PRETTY_PRINT));
+        DB::beginTransaction();
+
+        try {
+
+            $datatoupdate = [
+                'car_parking' => $req->car_parking,
+                'facing_direction' => $req->facing_direction,
+                'flat_each_floor' => $req->flat_each_floor,
+                'lifts_in_tower' => $req->lifts_in_tower,
+                'water_available' => $req->water_available,
+                'electric_available' => $req->electric_available,
+                'property_furnish' => $req->property_furnish,
+                'total_floor' => $req->total_floor,
+                'floor' => $req->floor_nnumber,
+                'expected_possesion_month_year' => trim($req->possesion_month . '-' . $req->possesion_year),
+                'possession_status' => $req->possession_status,
+                'construct_year' => $req->construct_year,
+                'buyer_message' => $req->buyer_message,
+                'kitchen' => $req->kitchen_count,
+            ];
+
+            DB::table('pref_property_additional')->where('pid', $req->property_id)->update($datatoupdate);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 0,
+                'message' => 'Failed to get property',
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+}
