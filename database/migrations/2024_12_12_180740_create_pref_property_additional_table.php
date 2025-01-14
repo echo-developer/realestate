8bfc790c773bd9ace64d282aeea23c95514cbd25
Soumyadip <143384729+echo-developer@users.noreply.github.com>
2025-01-14 13:15:56 +0530

    Update 2024_12_12_180740_create_pref_property_additional_table.php

--- database/migrations/2024_12_12_180740_create_pref_property_additional_table.php
+++ database/migrations/2024_12_12_180740_create_pref_property_additional_table.php
@@ -24,8 +24,7 @@ return new class extends Migration
             $table->text('property_amenity')->nullable();
             $table->integer('total_flats')->nullable();
             $table->integer('token_amount')->nullable();
-            $table->string('property_desc')->nullable();
-
+            $table->longText('property_desc')->nullable();
         });
     }
 
