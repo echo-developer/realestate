ea718862b8135e382248902b3a3351f6519b0038
Soumyadip <143384729+echo-developer@users.noreply.github.com>
2025-01-22 17:25:00 +0530

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -182,7 +182,6 @@ class AuthController extends Controller
         $request->validate([
             'token' => 'required',
             'email' => 'required|email',
-            'password' => 'required|confirmed|min:8',
         ]);
     
         $email = $request->email;
