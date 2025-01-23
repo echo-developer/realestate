9b27cd6e9b3a4edd8929f128ccdd5d8ef37cde8c
Moin Alam <143384729+echo-developer@users.noreply.github.com>
2025-01-23 19:06:11 +0530

    ypd

--- next-app/src/pages/login/index.js
+++ next-app/src/pages/login/index.js
@@ -16,7 +16,9 @@ const Index = () => {
   };
 
   const validationSchema = Yup.object({
-    email: Yup.string().required('email is required'),
+    email: Yup.string()
+          .email("Invalid email format")
+          .required("Email is required"),
     password: Yup.string()
       .required('Password is required')
       .min(6, 'Password must be at least 6 characters'),
