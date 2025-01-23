94f8bf664819f79f9bdd96646113ea69c6f5f60c
Soumyadip <143384729+echo-developer@users.noreply.github.com>
2025-01-23 15:07:53 +0530

    upd folder

---
+++ hackground/app/Http/Controllers/Api/Project/ProjectHomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Project;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectHomeController extends Controller
+{
+   function  GetProjects(){
+    
+   }
+}
