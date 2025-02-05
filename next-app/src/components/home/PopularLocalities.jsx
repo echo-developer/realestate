a942d60815e7b90648f67142ccc5a238efd1bb74
Bappaditya <143384729+echo-developer@users.noreply.github.com>
2025-02-05 17:58:29 +0530

    fix

--- next-app/src/components/home/PopularLocalities.jsx
+++ next-app/src/components/home/PopularLocalities.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+"use client";
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import AuthUser from '../Authentication/AuthUser';
 
 const PopularLocalities = () => {
+  const { callApi } = AuthUser();
+  const [activeTab, setActiveTab] = useState(null);
+  const [cityTabs, setCityTabs] = useState([]);
+  const [projectList, setProjectList] = useState([]);
   const localities = [
     { name: 'Rajarhat', image: 'assets/images/company/company-10.jpg', priceRange: '$34,999 - $39,999', reviews: 204 },
     { name: 'Newtown', image: 'assets/images/city/abu-dhabi.jpg', priceRange: '$30,000 - $35,000', reviews: 150 },
@@ -14,6 +20,56 @@ const PopularLocalities = () => {
     tablet: { breakpoint: { max: 1024, min: 768 }, items: 2, slidesToSlide: 1 },
     mobile: { breakpoint: { max: 768, min: 0 }, items: 1, slidesToSlide: 1 },
   };
+
+
+  const getCityTabs = async () => {
+    try {
+      const res = await callApi({
+        api: "/get_property_cities",
+        method: "GET"
+      })
+      if (res && res?.status === 1) {
+        setCityTabs(res?.data);
+      }
+    } catch (error) {
+      console.error(error?.message || "Something went wrong")
+    }
+  }
+
+
+  useEffect(() => {
+    getCityTabs();
+  }, [])
+
+
+  useEffect(() => {
+    if (cityTabs?.length > 0) {
+      setActiveTab(cityTabs[0]);
+    }
+  }, [cityTabs?.length])
+
+
+  useEffect(() => {
+    if (activeTab) {
+      const getProjectList = async () => {
+        try {
+          const res = await callApi({
+            api: `/project_list_by_city?city_id=${activeTab?.city_id  || 1}`,
+            method: "GET"
+          })
+
+          if (res && res?.status === 1) {
+            setProjectList(res?.data);
+          }
+        } catch (error) {
+          console.error(error?.message || "Something went wrong")
+        }
+      }
+
+
+      getProjectList();
+    }
+  }, [activeTab])
 
   return (
     <section className="section">
@@ -33,28 +89,29 @@ const PopularLocalities = () => {
             <div className="card h-100">
               <div className="card-header p-0">
                 <ul className="nav nav-underline nav-fill">
-                  {localities.map((locality, index) => (
-                    <li className="nav-item" key={index}>
-                      <a className="nav-link" href="#">
-                        {locality.name}
+                  {cityTabs?.length > 0 && cityTabs.map((locality, index) => (
+                    <li className="nav-item" key={index} onClick={() => setActiveTab(locality)}>
+                      <a className={`nav-link ${activeTab?.name === locality?.name ? "active" : ""}`} role="button">
+                        {locality?.name}
                       </a>
                     </li>
                   ))}
                 </ul>
               </div>
               <div className="card-body">
-                <Carousel
-                  responsive={responsive}
-                  swipeable={true}
-                  draggable={true}
-                  showDots={false}
-                  arrows={false}
-                  infinite={true}
-                  keyBoardControl={true}
-                  containerClass="carousel-container"
-                  itemClass="px-3"
-                >
-                  {localities.map((locality, index) => (
+                {projectList?.length > 0 && (
+                  <Carousel
+                    responsive={responsive}
+                    swipeable={true}
+                    draggable={true}
+                    showDots={false}
+                    arrows={false}
+                    infinite={true}
+                    keyBoardControl={true}
+                    containerClass="carousel-container"
+                    itemClass="px-3"
+                  >
+                    {/* {localities.map((locality, index) => (
                     <div key={index} className="card card-city">
                       <div className="card-body">
                         <div className="d-flex">
@@ -77,8 +134,43 @@ const PopularLocalities = () => {
                         </div>
                       </div>
                     </div>
-                  ))}
-                </Carousel>
+                  ))} */}
+                    {projectList?.map((project, i) => {
+                      const firstImage = project?.gallery?.length > 0 ? project?.gallery[0]?.images[0] : "";
+                      return (
+                        <div key={i} className="card card-city">
+                          <div className="card-body">
+                            <div className="d-flex">
+                              <img
+                                src={firstImage?.file}
+                                alt="Locality Name"
+                                height="64"
+                                width="64"
+                                className="rounded-circle"
+                              />
+                              <div className="flex-grow-1 ps-3">
+                                <h4>
+                                  {project?.project_name}
+                                  <a href=""><i className="bi bi-box-arrow-up-right"></i></a>
+                                </h4>
+                                <h5>$500,000 - $800,000</h5>
+                                <p>
+                                  {project?.stars && (
+                                    <>
+                                      <i className="bi bi-star-fill text-warning"></i> {project.stars} &nbsp; | &nbsp; {project.reviews}
+                                    </>
+                                  )}
+
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                      )
+                    })}
+                  </Carousel>
+                )}
               </div>
             </div>
           </aside>
