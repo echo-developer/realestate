f409151e5438190f20a928823ad05818ac7d24dc
Soumyadip <143384729+echo-developer@users.noreply.github.com>
2025-02-07 12:14:14 +0530

    Update ProjectPropertyController.php

--- hackground/app/Http/Controllers/Api/Project/ProjectPropertyController.php
+++ hackground/app/Http/Controllers/Api/Project/ProjectPropertyController.php
@@ -63,6 +63,7 @@ class ProjectPropertyController extends Controller
                     $tower = ProjectProperties::updateOrCreate(
                         [
                             'project_id' => $project_id,
+                            'tower_name' => $items['tower_name']
                         ],
                         [
                             'tower_name' => $items['tower_name'],
