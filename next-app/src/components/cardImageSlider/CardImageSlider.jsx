efebec24b81020aa4bda4bba00d9599e8849d2b9
Bappaditya <143384729+echo-developer@users.noreply.github.com>
2025-02-26 19:59:05 +0530

    fix property list

--- next-app/src/components/cardImageSlider/CardImageSlider.jsx
+++ next-app/src/components/cardImageSlider/CardImageSlider.jsx
@@ -106,6 +106,9 @@ const CardImageSlider = ({ data, keyword, id, addRemoveFav, mainType, showSq, ic
         </>
       )}
       {data?.expected_price && (<h4 className="ads-price">{data?.currency} {data.expected_price}</h4>)}
+      {data?.price && (
+        <h4 className="ads-price">{data.price}</h4>
+      )}
     </div>
   );
 };
