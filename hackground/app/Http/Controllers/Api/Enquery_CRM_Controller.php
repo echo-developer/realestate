cd582f162232443029aca117ce90a549e52b1908
Soumyadip <143384729+echo-developer@users.noreply.github.com>
2025-04-07 13:27:40 +0530

    Update Enquery_CRM_Controller.php

--- hackground/app/Http/Controllers/Api/Enquery_CRM_Controller.php
+++ hackground/app/Http/Controllers/Api/Enquery_CRM_Controller.php
@@ -714,7 +714,8 @@ class Enquery_CRM_Controller extends Controller
                         'current_page' => $recentPage,
                         'total_records' => $totalRecords,
                         'total_pages' => ceil($totalRecords / $limit),
-                    ]
+                    ],
+                    'enquery_can_show' => (int) get_remaining_values('owner_contacted',$user_id),
                 ]);
             } else {
                 return response()->json([
