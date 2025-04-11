a5f252b2380c310fbe8df36eb358103f4b7357c1
Soumyadip <143384729+echo-developer@users.noreply.github.com>
2025-04-11 13:18:21 +0530

    email fixed reset password and email verify

--- hackground/app/Jobs/SendEmailJob.php
+++ hackground/app/Jobs/SendEmailJob.php
@@ -1,15 +1,11 @@
 <?php
 
 namespace App\Jobs;
-
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
 class SendEmailJob implements ShouldQueue
 {
