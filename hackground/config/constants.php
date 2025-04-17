f649ff216609ee53c3da96312ee8d74fde09c0c5
Dev <55586540+originate-soft@users.noreply.github.com>
2025-04-17 18:18:11 +0530

    Update constants.php

--- hackground/config/constants.php
+++ hackground/config/constants.php
@@ -6,5 +6,6 @@ return [
     'STATUS_DELETE' => '-1',
     'STATUS_DRAFT' => '2',
     'NO_IMAGE' => 'assets/icons/no_image.png',
+    'NO_IMAGE_PROPERTY' => 'assets/images/no-image.jpg',
     'LOCALITY_EXCEL_FORMAT' => 'assets/defaults/locality_excel_format/City_Localities_With_LatLng - Copy.xlsx',
 ];
