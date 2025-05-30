4fcc686d45fae0ca690ad3affdc96d21dd0cd0e8
echo-developer <143384729+echo-developer@users.noreply.github.com>
2025-05-30 19:52:22 +0530

    commit

--- hackground/app/Models/ProjectReports.php
+++ hackground/app/Models/ProjectReports.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Prefproject;
+use App\Models\PrefProject;
 
 class ProjectReports extends Model
 {
