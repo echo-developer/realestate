54eab482a892846193b18800d7fe63d8c3092510
Bappaditya <143384729+echo-developer@users.noreply.github.com>
2025-09-13 13:25:21 +0530

    up

--- next-app/src/pages/_document.js
+++ next-app/src/pages/_document.js
@@ -33,6 +33,15 @@ class MyDocument extends Document {
             type="text/css"
             rel="stylesheet"
           />
+
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
+            integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
+            crossorigin="anonymous"
+          />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.min.css" />
         </Head>
         <body >
           <Main />
